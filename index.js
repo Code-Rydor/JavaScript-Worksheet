251ad262c7d50adf56225480aec1dc52506c6ea6
Garrett <ga.landin89@gmail.com>
2022-03-25 15:45:39 -0500

    Created index.html and index.js files + Finished Variables section in worksheet and problem 1 in Conditionals section

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+ 
+// VARIABLES //
+
+// // 1 //
+// let dayOfWeek = "Monday";
+// console.log(dayOfWeek);
+// dayOfWeek = "Friday";
+// console.log(`I cant wait for ${dayOfWeek}!`);
+
+// // 2 //
+// let animalInput = prompt("What is your favorite animal?");
+// let colorInput = prompt("What is your favorite color?");
+// console.log(`Ive never seen a ${colorInput} ${animalInput}!`);
+
+// CONDITIONALS //
+
+// 1 //
+// Breakfast - Steak Omelette, Lunch - Turkey Sandwich, Dinner - Steak
+let timeOfDay = elephant;
+let chosenMeal;
+if (timeOfDay < 1200) {
+    chosenMeal = "Steak Omelette";
+} else if (timeOfDay >= 1200 && timeOfDay <= 1700) {
+    chosenMeal = "Turkey Sandwich";
+} else if (timeOfDay > 1700) {
+    chosenMeal = "Steak";
+}
+console.log(chosenMeal);
+
+
+
