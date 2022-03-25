33956e803c1187174d31f7259020c9af873b1cf5
Garrett <ga.landin89@gmail.com>
2022-03-25 16:45:27 -0500

    Finished Conditionals section + Finished first 2 problems in Loops section

--- index.js
+++ index.js
@@ -15,18 +15,41 @@
 
 // CONDITIONALS //
 
-// 1 //
-// Breakfast - Steak Omelette, Lunch - Turkey Sandwich, Dinner - Steak
-let timeOfDay = elephant;
-let chosenMeal;
-if (timeOfDay < 1200) {
-    chosenMeal = "Steak Omelette";
-} else if (timeOfDay >= 1200 && timeOfDay <= 1700) {
-    chosenMeal = "Turkey Sandwich";
-} else if (timeOfDay > 1700) {
-    chosenMeal = "Steak";
-}
-console.log(chosenMeal);
+// // 1 //
+// // Breakfast - Steak Omelette, Lunch - Turkey Sandwich, Dinner - Steak
+// let timeOfDay = elephant;
+// let chosenMeal;
+// if (timeOfDay < 1200) {
+//     chosenMeal = "Steak Omelette";
+// } else if (timeOfDay >= 1200 && timeOfDay <= 1700) {
+//     chosenMeal = "Turkey Sandwich";
+// } else if (timeOfDay > 1700) {
+//     chosenMeal = "Steak";
+// }
+// console.log(chosenMeal);
 
+// // 2 //
 
+// let randomNumber = Math.floor(Math.random() * 11);
+// if (randomNumber >= 0 && randomNumber <= 2) {
+//     console.log("Beatles");
+// } else if (randomNumber > 2 && randomNumber < 6) {
+//     console.log("Stones");
+// } else if (randomNumber >= 6 && randomNumber <= 8) {
+//     console.log("Floyd");
+// } else if (randomNumber == 9 || randomNumber == 10) {
+//     console.log("Hendrix");
+// }
 
+// LOOPS //
+
+// // 1 //
+
+// for (let index = 0; index < 7; index++) {
+//     console.log("JavaScript is cool!");    
+// }
+
+// // 2 //
+
+// for (let index = 0; index < 11; index++)
+// console.log(index);
